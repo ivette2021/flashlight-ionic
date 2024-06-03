00926eaa73efbe88c97a590cfef5d0e5e25a3300
iconsWork <iconstanzav1@gmail.com>
2024-06-03 18:01:49 -0400

    primera parte

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public active: boolean;
 
-  constructor() {}
-
+  constructor() {
+    this.active = false;
+  }
 }
